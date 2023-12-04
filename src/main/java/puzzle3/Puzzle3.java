8081770f86da14c2606ccebc784527accce6f457
L. K <LK00100100@gmail.com>
2023-12-04 01:28:01 -0500

    solved puzzle 3-2 quickly

--- src/main/java/puzzle3/Puzzle3.java
+++ src/main/java/puzzle3/Puzzle3.java
@@ -7,7 +7,8 @@ import java.util.*;
 public class Puzzle3 {
 
     /**
-    /**
+     * /**
+     *
      * @param file assume valid
      * @return list of rows
      */
@@ -29,8 +30,7 @@ public class Puzzle3 {
     }
 
     /**
-     *
-     * @param rows -
+     * @param rows   -
      * @param rowIdx -
      * @return sum of numbers next to symbol
      */
@@ -39,27 +39,26 @@ public class Puzzle3 {
 
         //parse these rows for numbers
         int total = 0;
-        for(int colIdx = 0; colIdx < rows.size(); colIdx++) {
+        for (int colIdx = 0; colIdx < rows.size(); colIdx++) {
             char c = row.charAt(colIdx);
 
             int lastDigitIdx = colIdx; //exclusive
 
             //is this the start of a number?
-            if(Character.isDigit(c)) {
+            if (Character.isDigit(c)) {
                 //parse the entire number
-                while(++lastDigitIdx < rows.size()) {
+                while (++lastDigitIdx < rows.size()) {
                     char d = row.charAt(lastDigitIdx);
 
-                    if(!Character.isDigit(d))
+                    if (!Character.isDigit(d))
                         break;
                 }
-            }
-            else {
+            } else {
                 continue;
             }
 
             //is this number next to a symbol? (8-directional)
-            if(isNextToSymbol(rows, rowIdx, colIdx, lastDigitIdx - 1)) {
+            if (isNextToSymbol(rows, rowIdx, colIdx, lastDigitIdx - 1)) {
                 total += Integer.parseInt(row.substring(colIdx, lastDigitIdx));
             }
 
@@ -69,65 +68,165 @@ public class Puzzle3 {
         return total;
     }
 
+    //dev note: just hackin' away. perhaps we can find the gear and then find the numbers
+
     /**
+     * assign number to gear
      *
-     * @param rows -
-     * @param row number's row
+     * @param rows    -
+     * @param rowIdx  -
+     * @param gearMap 'row,col of gear' key with multiple value
+     * @return sum of numbers next to symbol
+     */
+    public static int parseGearNumber(List<String> rows, int rowIdx, Map<String, List<Integer>> gearMap) {
+        String row = rows.get(rowIdx);
+
+        //parse these rows for numbers
+        int total = 0;
+        for (int colIdx = 0; colIdx < rows.size(); colIdx++) {
+            char c = row.charAt(colIdx);
+
+            int lastDigitIdx = colIdx; //exclusive
+
+            //is this the start of a number?
+            if (Character.isDigit(c)) {
+                //parse the entire number
+                while (++lastDigitIdx < rows.size()) {
+                    char d = row.charAt(lastDigitIdx);
+
+                    if (!Character.isDigit(d))
+                        break;
+                }
+            } else {
+                continue;
+            }
+
+            //is this number next to a symbol? (8-directional)
+            var gearCoordinateOpt = isNextToGear(rows, rowIdx, colIdx, lastDigitIdx - 1);
+            if (gearCoordinateOpt.isPresent()) {
+                int number = Integer.parseInt(row.substring(colIdx, lastDigitIdx));
+
+                var gearCoordinate = gearCoordinateOpt.get();
+                if (!gearMap.containsKey(gearCoordinate)) {
+                    gearMap.put(gearCoordinate, new LinkedList<>());
+                }
+
+                var gearNumbers = gearMap.get(gearCoordinate);
+                gearNumbers.add(number);
+            }
+
+            colIdx = lastDigitIdx - 1;
+        }
+
+        return total;
+    }
+
+    /**
+     * @param rows     -
+     * @param row      number's row
      * @param colStart inclusive
-     * @param colEnd inclusive
+     * @param colEnd   inclusive
      * @return true if next to symbol (8-directional)
      */
     private static boolean isNextToSymbol(List<String> rows, int row, int colStart, int colEnd) {
         //check top row
-        for(int col = colStart - 1; col <= colEnd + 1; col++) {
-            if(isSymbol(rows, row - 1, col))
+        for (int col = colStart - 1; col <= colEnd + 1; col++) {
+            if (isSymbol(rows, row - 1, col))
                 return true;
         }
 
         //check bottom row
-        for(int col = colStart - 1; col <= colEnd + 1; col++) {
-            if(isSymbol(rows, row + 1, col))
+        for (int col = colStart - 1; col <= colEnd + 1; col++) {
+            if (isSymbol(rows, row + 1, col))
                 return true;
         }
 
         //check to the sides of the number
-        if(isSymbol(rows, row, colStart - 1))
+        if (isSymbol(rows, row, colStart - 1))
             return true;
 
-        if(isSymbol(rows, row, colEnd + 1))
+        if (isSymbol(rows, row, colEnd + 1))
             return true;
 
         return false;
     }
 
     /**
+     * returns one gear (assumed one adjacent gear per number)
+     *
+     * @param rows     -
+     * @param row      number's row
+     * @param colStart inclusive
+     * @param colEnd   inclusive
+     * @return returns 'row,col' or Optional.empty if no gear
+     */
+    private static Optional<String> isNextToGear(List<String> rows, int row, int colStart, int colEnd) {
+        //check top row
+        for (int col = colStart - 1; col <= colEnd + 1; col++) {
+            if (isGear(rows, row - 1, col))
+                return Optional.of((row - 1) + "," + col);
+        }
+
+        //check bottom row
+        for (int col = colStart - 1; col <= colEnd + 1; col++) {
+            if (isGear(rows, row + 1, col))
+                return Optional.of((row + 1) + "," + col);
+        }
+
+        //check to the sides of the number
+        if (isGear(rows, row, colStart - 1))
+            return Optional.of(row + "," + (colStart - 1));
+
+        if (isGear(rows, row, colEnd + 1))
+            return Optional.of(row + "," + (colEnd + 1));
+
+        return Optional.empty();
+    }
+
+    /**
      * is there a symbol here? out of bounds is false.
+     *
      * @return true if so.
      */
     private static boolean isSymbol(List<String> rows, int row, int col) {
-        if(isOutOfBounds(rows, row, col))
+        if (isOutOfBounds(rows, row, col))
             return false;
 
         char c = rows.get(row).charAt(col);
 
-        if(Character.isDigit(c))
+        if (Character.isDigit(c))
             return false;
 
         return c != '.';
     }
 
     /**
+     * is there a '*' here? out of bounds is false.
+     *
+     * @return true if so.
+     */
+    private static boolean isGear(List<String> rows, int row, int col) {
+        if (isOutOfBounds(rows, row, col))
+            return false;
+
+        char c = rows.get(row).charAt(col);
+
+        return c == '*';
+    }
+
+    /**
      * true if out of bounds. Otherwise, false.
+     *
      * @param rows -
-     * @param row -
-     * @param col -
+     * @param row  -
+     * @param col  -
      * @return -
      */
     private static boolean isOutOfBounds(List<String> rows, int row, int col) {
-        if(row < 0 || row >= rows.size())
+        if (row < 0 || row >= rows.size())
             return true;
 
-        if(col < 0 || col >= rows.get(row).length())
+        if (col < 0 || col >= rows.get(row).length())
             return true;
 
         return false;
--- src/test/java/Puzzle3Test.java
+++ src/test/java/Puzzle3Test.java
@@ -5,6 +5,7 @@ import puzzle3.Puzzle3;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
@@ -40,6 +41,52 @@ class Puzzle3Test {
         }
 
         assertEquals(540212, sum);
+    }
+
+    @Test
+    void solvePart2Sample() throws FileNotFoundException {
+        File inputFile = new File(Objects.requireNonNull(this.getClass().getResource("puzzle3-sample.txt")).getFile());
+
+        List<String> rows = Puzzle3.readTokens(inputFile);
+
+        Map<String, List<Integer>> gearMap = new HashMap<>();
+
+        //get gear ratios
+        for (int i = 0; i < rows.size(); i++) {
+            Puzzle3.parseGearNumber(rows, i, gearMap);
+        }
+
+        //sum only gears with 2 numbers
+        int sum = gearMap.values().stream()
+                .filter(list -> list.size() == 2)
+                .map(list -> list.get(0) * list.get(1))
+                .reduce(Integer::sum).orElse(0);
+        System.out.println(sum);
+
+        assertEquals(467835, sum);
+    }
+
+    @Test
+    void solvePart2() throws FileNotFoundException {
+        File inputFile = new File(Objects.requireNonNull(this.getClass().getResource("puzzle3.txt")).getFile());
+
+        List<String> rows = Puzzle3.readTokens(inputFile);
+
+        Map<String, List<Integer>> gearMap = new HashMap<>();
+
+        //get gear ratios
+        for (int i = 0; i < rows.size(); i++) {
+            Puzzle3.parseGearNumber(rows, i, gearMap);
+        }
+
+        //sum only gears with 2 numbers
+        int sum = gearMap.values().stream()
+                .filter(list -> list.size() == 2)
+                .map(list -> list.get(0) * list.get(1))
+                .reduce(Integer::sum).orElse(0);
+        System.out.println(sum);
+
+        assertEquals(87605697, sum);
     }
 
 }
