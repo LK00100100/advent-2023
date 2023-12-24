867df3b32aaab223846f9f58f3adf9243cf051d5
L. K <LK00100100@gmail.com>
2023-12-24 14:43:49 -0500

    puzzle 8 part 2 solution

--- src/main/java/puzzle8/Network.java
+++ src/main/java/puzzle8/Network.java
@@ -1,15 +1,13 @@
 package puzzle8;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 //nodes = nodeid, Node
 public record Network(String instructions, Map<String, Node> nodes) {
 
     //from start, how many steps? (use instructions, repeated)
-    public int countStepsFromAAAToZZZ() {
-        Node current = nodes.get("AAA");
+    public int countStepsFromSourceToZZZ(String nodeId) {
+        Node current = nodes.get(nodeId);
 
         int numSteps = 0;
 
@@ -37,6 +35,49 @@ public record Network(String instructions, Map<String, Node> nodes) {
         }
     }
 
+    /**
+     * from start, how many steps? (use instructions, repeated)
+     * cycle will end on something ending in Z
+     * A -> B -> C -> Z -> B (etc) will yield [a, b, c, z, b, c, z]
+     * @param nodeId -
+     * @return -
+     */
+    public List<String> getNodesFromSourceCycle(String nodeId) {
+        Set<String> visited = new HashSet<>();
+        List<String> path = new ArrayList<>();
+
+        Node current = nodes.get(nodeId);
+
+        //could be infinite :-/
+        while (true) {
+            for (int i = 0; i < instructions.length(); i++) {
+                char c = instructions.charAt(i);
+
+                String key = i + ";" + current.id();
+
+                if(visited.contains(key) && current.id().endsWith("Z")) {
+                    path.add(current.id());
+                    return path;
+                }
+
+                visited.add(key);
+                path.add(current.id());
+
+                //go left
+                if (c == 'L') {
+                    current = nodes.get(current.leftId());
+                }
+                //go right
+                else if (c == 'R') {
+                    current = nodes.get(current.rightId());
+                }
+                else {
+                    throw new IllegalArgumentException("hmmm...");
+                }
+
+            }
+        }
+    }
 
     //from start, how many steps? (use instructions, repeated)
     public int getNumStepsFromEndingAtoAllEndingZ() {
@@ -83,5 +124,49 @@ public record Network(String instructions, Map<String, Node> nodes) {
                 temp.clear();
             }
         }
+    }
+
+    public List<String> listNodesEndingWithA() {
+        return this.nodes.keySet().stream()
+                .filter(x -> x.endsWith("A"))
+                .toList();
+    }
+
+    /**
+     * nodes doesn't start on Z.
+     *
+     * @param nodes -
+     * @return only use the first step once. probably should be safety rails...
+     */
+    public static List<Integer> listToStepToNodeEndingWithZCount(List<String> nodes) {
+        List<Integer> stepCounts = new ArrayList<>();
+
+        //AAA, BBB, CCC, ZZZ, 111, 222
+        //should be: just 3, 5 - use first node once
+
+        //AAA, BBB, CCC, ZZZ, 111, 222, ZZZ, AAA
+        //should be: just 3, 3, 4 - use first node once
+
+        //for every current node, when is the next --Z node?
+        for(int current = 0; current < nodes.size(); current++) {
+            for(int numSteps = 1; current + numSteps < nodes.size(); numSteps++) {
+                if(nodes.get(current + numSteps).endsWith("Z")) {
+                    stepCounts.add(numSteps);
+                    current = current + numSteps - 1;   //start on --Z
+                    break;
+                }
+
+                //at the end but we have these extra steps...
+                if(current + numSteps == nodes.size() - 1) {
+                    stepCounts.add(stepCounts.getLast() + numSteps);
+                    current = current + numSteps;
+                }
+            }
+        }
+
+        if(stepCounts.size() == 1)
+            stepCounts.add(stepCounts.get(0));
+
+        return stepCounts;
     }
 }
--- src/test/java/Puzzle8Test.java
+++ src/test/java/Puzzle8Test.java
@@ -1,8 +1,12 @@
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import puzzle8.Network;
 import puzzle8.Puzzle8;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -15,7 +19,7 @@ class Puzzle8Test {
 
         var network = Puzzle8.readTokens(inputFile);
 
-        assertEquals(6, network.countStepsFromAAAToZZZ());
+        assertEquals(6, network.countStepsFromSourceToZZZ("AAA"));
     }
 
     @Test
@@ -24,7 +28,7 @@ class Puzzle8Test {
 
         var network = Puzzle8.readTokens(inputFile);
 
-        assertEquals(18673, network.countStepsFromAAAToZZZ());
+        assertEquals(18673, network.countStepsFromSourceToZZZ("AAA"));
     }
 
     @Test
@@ -35,13 +39,110 @@ class Puzzle8Test {
 
         assertEquals(6, network.getNumStepsFromEndingAtoAllEndingZ());
     }
+
+    @Disabled("slow")
     @Test
-    void solvePart2() throws FileNotFoundException {
+    void solvePart2Slow() throws FileNotFoundException {
         File inputFile = new File(Objects.requireNonNull(this.getClass().getResource("puzzle8.txt")).getFile());
 
         var network = Puzzle8.readTokens(inputFile);
 
-        assertEquals(6, network.getNumStepsFromEndingAtoAllEndingZ());
+        var answer = network.nodes().keySet().stream()
+                .filter(nodeId -> nodeId.endsWith("A"))
+                .map(network::countStepsFromSourceToZZZ)
+                .reduce((a, b) -> a * b)
+                .orElse(0);
+
+        assertEquals(6, answer);
+    }
+
+    @Test
+    void shouldReturnValueNumSteps() throws FileNotFoundException {
+        //AAA, BBB, CCC, ZZZ, 111, 222
+        //should be: just 3, 5 -
+
+        //AAA, BBB, CCC, ZZZ, 111, 222, ZZZ, AAA
+        //should be: just 3, 3, 5 - use first node once
+
+        //AAA, BBB, CCC, ZZZ
+        //should be: just 3, 3
+
+        //AAA, BBB, CCC, ZZZ, CCC
+        //should be: just 3, 2 (to infinity)
+
+        List<String> startingNodes = List.of("AAA", "BBB", "CCC", "ZZZ", "111", "222");
+        List<String> startingNodes2 = List.of("AAA", "BBB", "CCC", "ZZZ", "111", "222", "ZZZ", "AAA");
+        List<String> startingNodes3 = List.of("AAA", "BBB", "CCC", "ZZZ");
+
+        var stepList = Network.listToStepToNodeEndingWithZCount(startingNodes);
+        var stepList2 = Network.listToStepToNodeEndingWithZCount(startingNodes2);
+        var stepList3 = Network.listToStepToNodeEndingWithZCount(startingNodes3);
+
+        assertEquals(List.of(3, 5), stepList);
+        assertEquals(List.of(3, 3, 4), stepList2);
+        assertEquals(List.of(3, 3), stepList3);
+    }
+
+    @Test
+    void solvePart2() throws FileNotFoundException {
+        //File inputFile = new File(Objects.requireNonNull(this.getClass().getResource("puzzle8-2-sample.txt")).getFile());
+        File inputFile = new File(Objects.requireNonNull(this.getClass().getResource("puzzle8.txt")).getFile());
+
+        var network = Puzzle8.readTokens(inputFile);
+
+        var nodesStartingWithA = network.listNodesEndingWithA();
+
+        var nodeCycles = nodesStartingWithA.stream().map(network::getNodesFromSourceCycle).toList();
+
+        //observation, first hit to Z is the same distance as the second hit to Z.
+        var nodeNextZPath = nodeCycles.stream().map(Network::listToStepToNodeEndingWithZCount).toList();
+
+        //all of the nodes, for the first and second node (steps to Z), for my input, look the same.
+        //im going off of goofy observations with my particular input
+        //not generic code.
+
+        //these are actually 6 paths that are 2 nodes deep. we skip the first node (it won't be needed)
+
+        //yolo code below
+
+        //simulate cycles
+        int nextZ = nodeNextZPath.stream().map(x -> x.get(0)).reduce(Integer::min).orElseThrow();
+
+        int[] stepsLeftToNextZ = nodeNextZPath.stream().map(vals -> vals.get(0))
+                .mapToInt(i -> i)
+                .toArray();
+
+        long numSteps = 0;
+        while (true) {
+            numSteps += nextZ;
+
+            int onZCount = 0;
+            //take a step per node-next-z path
+            //dont reuse first node
+            for (int i = 0; i < nodeNextZPath.size(); i++) {
+                if (nextZ <= stepsLeftToNextZ[i]) {
+                    stepsLeftToNextZ[i] = stepsLeftToNextZ[i] - nextZ;
+                } else {
+                    //min is used to subtract so you only subtract once.
+                    int diff = nextZ - stepsLeftToNextZ[i];
+                    stepsLeftToNextZ[i] = nodeNextZPath.get(i).get(1);
+                    stepsLeftToNextZ[i] = stepsLeftToNextZ[i] - diff;
+                }
+
+                if (stepsLeftToNextZ[i] == 0) {
+                    onZCount++;
+                }
+            }
+
+            if (onZCount == nodeNextZPath.size()) {
+                System.out.println("numSteps: " + numSteps);
+                assertEquals(17972669116327L, numSteps;
+                break;
+            }
+
+            nextZ = Arrays.stream(stepsLeftToNextZ).filter(x -> x != 0).reduce(Integer::min).orElseThrow();  //then use 12362    //min from nodeNextZPath
+        }
+
     }
 
 }
